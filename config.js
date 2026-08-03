eed0cdc7946670656ee6ba46b01340eec42dce8b
rugbyniversaire <rugbyniversaire@gmail.com>
2026-08-03 09:38:27 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,3 +4,7 @@ const SUPABASE_URL = "https://TON-PROJET.supabase.co";
 const SUPABASE_ANON_KEY = "TA_CLE_ANON_ICI";
 
 const CATEGORIES = ["Photo", "Musique", "Lieu", "Autre"];
+
+// Mot de passe pour pouvoir ajouter/supprimer des participants.
+// Choisis-en un simple mais que les autres ne devineront pas facilement.
+const ADMIN_PASSWORD = "change-moi";
